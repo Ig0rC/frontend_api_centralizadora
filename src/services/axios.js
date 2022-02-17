d31a31fe2e26b39d705d4a740197a2bb246aa865
Igor Cardoso <49351266+Ig0rC@users.noreply.github.com>
2022-02-17 15:44:44 -0300

    Finalizado

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:5000/api-gestor/',
+  baseURL: 'https://api-centralizador.softvendas.com.br/api-gestor/',
 });
